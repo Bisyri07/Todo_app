00e044644727c97d36b54f5f208cbd489375d089
bisyri 08 <mbisyri33@gmail.com>
2024-11-18 20:15:25 +0700

    Seperate each task as subcomponent

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const { Component, mount, xml, useState } = owl;
 
 
+// class for subcomponent (child)
+class Task extends Component {
+    static template = xml`
+    <li t-attf-style="background-color:#{props.task.color}" class="d-flex align-items-center justify-content-between border p-3 mb-2 rounded">
+        <div class="form-check form-switch fs-5">
+            <input class="form-check-input" type="checkbox" value="" t-att-id="props.task.id"/>
+            <label class="form-check-label" t-att-for="props.task.id">
+                <t t-esc="props.task.name"/>
+            </label>
+        </div>
+        <div>
+            <button class="btn btn-primary me-2"><i class="bi bi-pencil fs-5"></i></button>
+            <button class="btn btn-danger"><i class="bi bi-trash fs-5"></i></button>
+        </div>
+    </li>
+    `
+    static props = ["task"]
+}
+
+
 // class inheritance
 class Root extends Component {
     static template = xml`
@@ -21,21 +41,12 @@ class Root extends Component {
 
     <ul class="d-flex flex-column mt-5 p-0">
         <t t-foreach="tasks" t-as="task" t-key="task.id">
-            <li t-attf-style="background-color:#{task.color}" class="d-flex align-items-center justify-content-between border p-3 mb-2 rounded">
-                <div class="form-check form-switch fs-5">
-                    <input class="form-check-input" type="checkbox" value="" t-att-id="task.id"/>
-                    <label class="form-check-label" t-att-for="task.id">
-                        <t t-esc="task.name"/>
-                    </label>
-                </div>
-                <div>
-                    <button class="btn btn-primary me-2"><i class="bi bi-pencil fs-5"></i></button>
-                    <button class="btn btn-danger"><i class="bi bi-trash fs-5"></i></button>
-                </div>
-            </li>
+            <Task task="task"/>
         </t>
     </ul>
     `
+
+    static components = {Task}
 
     // life cycle method similar with __init__() in python
     setup(){
